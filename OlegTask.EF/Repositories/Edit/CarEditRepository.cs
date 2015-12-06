3143f1ff02069ade57c5254892075eb8c1b6e6d8
Vasilev Pyotr <vahpetr@gmail.com>
2015-12-06 23:16:37 +0500

    * added check at removing car by exist any car on driver

--- OlegTask.EF/Repositories/Edit/CarEditRepository.cs
+++ OlegTask.EF/Repositories/Edit/CarEditRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System.Data.Entity;
+using System.Linq;
 using Common.EF.Repositories;
+using Common.Exceptions;
 using OlegTask.Helpers;
 using OlegTask.Models;
 using OlegTask.Repositories.Edit.Contract;
@@ -14,6 +16,15 @@ namespace OlegTask.EF.Repositories.Edit
 
         public override void Remove(Car car)
         {
+            var driver = dbContext
+                .Set<Driver>()
+                .Include(p => p.Cars)
+                .AsNoTracking()
+                .FirstOrDefault(p => p.Cars.Any(b => b.Id == car.Id) && p.Cars.Count() == 1);
+
+            if (driver != null)
+                throw new ValidationServiceException($"Водитель {driver.Surname} {driver.Name} останется без машины");
+
             //каскадное удаление отключил специально, на случай чтобы ничего лишнего случайно не удалить
             var query = DriversCars.Detach(car.Id);
             dbContext.Database.ExecuteSqlCommand(query);
